49d430fe2311b3aad7c165ff7c9e2dbca2438efc
yurikars <buddy.yurik@gmail.com>
2017-12-18 23:49:05 +0300

    first commit

---
+++ src/test/java/MobileWebTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class MobileWebTest {
+
+    @Test
+    public void testIncorrectFBLogin(){
+        System.out.println("Step 1: Create new driver");
+        System.out.println("Step 2: Open website");
+        System.out.println("Step 3: Enter login");
+        System.out.println("Step 4: Enter password");
+        System.out.println("Step 5: Click login button");
+        System.out.println("Step 6: Check error message");
+        System.out.println("Step 7: Close");
+    }
+}
