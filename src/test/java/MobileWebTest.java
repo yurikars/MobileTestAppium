b72ef823924a2db956017201cc30e28cd16dc178
yurikars <buddy.yurik@gmail.com>
2017-12-19 00:03:48 +0300

    gitignore commit

--- src/test/java/MobileWebTest.java
+++ src/test/java/MobileWebTest.java
@@ -1,9 +1,13 @@
 import org.junit.Test;
 
+import java.net.URL;
+
 public class MobileWebTest {
 
     @Test
-    public void testIncorrectFBLogin(){
+    public void testIncorrectFBLogin()throws Exception {
+        URL serverURL = new URL("http://127.0.0.1:4723/wd/hub");
+
         System.out.println("Step 1: Create new driver");
         System.out.println("Step 2: Open website");
         System.out.println("Step 3: Enter login");
