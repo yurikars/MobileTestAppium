5eb9c91a64beed77e8c6b7d4d75ee46dbac7b3ca
yurikars <buddy.yurik@gmail.com>
2017-12-19 10:40:48 +0300

    example commit

--- src/test/java/MobileWebTest.java
+++ src/test/java/MobileWebTest.java
@@ -1,19 +1,51 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileBrowserType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
-public class MobileWebTest {
-
+public class MobileWebTest
+{
     @Test
-    public void testIncorrectFBLogin()throws Exception {
+    public void testIncorrectFBLogin () throws Exception {
         URL serverURL = new URL("http://127.0.0.1:4723/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        System.out.println("Step 1: Create new driver");
-        System.out.println("Step 2: Open website");
-        System.out.println("Step 3: Enter login");
-        System.out.println("Step 4: Enter password");
-        System.out.println("Step 5: Click login button");
-        System.out.println("Step 6: Check error message");
-        System.out.println("Step 7: Close");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"7.1.1");
+
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
+        //capabilities.setCapability((MobileCapabilityType.BROWSER_VERSION, "latest");
+        //capabilities.setCapability(MobileCapabilityType.FULL_RESET, true);
+
+        System.out.println ("Step 1: Create new driver");
+        AppiumDriver driver = new AndroidDriver(serverURL, capabilities);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        System.out.println ("Step 2: Open website");
+        driver.get("https://facebook.com");
+
+        System.out.println ("Step 3: Enter login");
+        driver.findElement(By.name("email")).sendKeys("max@test.com");
+
+        System.out.println ("Step 4: Enter password");
+        driver.findElement(By.name("pass")).sendKeys("12345");
+
+        System.out.println ("Step 5: Click login button");
+        driver.findElement(By.name("login")).click();
+
+        //System.out.println ("Step 6: Error message check");
+        //Assert.assertTrue(driver.findElement(By.cssSelector("div[data-sigil='m_login_notice']")).getText().contains("The password you entered is incorrect. "));
+
+        System.out.println ("Step 7: Close driver");
+        driver.quit();
     }
 }
